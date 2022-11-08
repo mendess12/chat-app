f6a096d6ab54f68547ed25f13e641dbc2d263d57
YUSUF\yusuf <yusufmendes123@gmail.com>
2022-11-08 17:33:01 +0300

    Kullanıcı izinleri için işlemler tamamlandı.

--- app/src/main/java/com/example/socialmedia/ui/home/AddPostFragment.kt
+++ app/src/main/java/com/example/socialmedia/ui/home/AddPostFragment.kt
@@ -1,13 +1,31 @@
 package com.example.socialmedia.ui.home
 
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.socialmedia.R
+import com.example.socialmedia.databinding.FragmentAddPostBinding
+import com.google.android.material.snackbar.Snackbar
 
 class AddPostFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddPostBinding
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var selectedPicture: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,4 +39,71 @@ class AddPostFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_add_post, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentAddPostBinding.bind(view)
+
+        registerLauncher()
+
+        binding.addPostImageView.setOnClickListener {
+
+            //izin işlemleri
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                ) {
+                    Snackbar.make(view, "Permission neede for gallery", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Give Permission") {
+
+                            //request permission
+                            permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        }.show()
+                } else {
+                    //request permission
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            } else {
+
+                //go to gallery
+                val intentToGallery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }
+        }
+    }
+
+    private fun registerLauncher() {
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+
+                if (result.resultCode == RESULT_OK) {
+                    val intentFromResult = result.data
+                    if (intentFromResult != null) {
+                        selectedPicture = intentFromResult.data
+                        selectedPicture?.let {
+                            binding.addPostImageView.setImageURI(it)
+                        }
+                    }
+                }
+            }
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+                if (result) {
+                    //permission granted
+                    val intentToGallery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intentToGallery)
+                } else {
+                    //permission denied
+                    Toast.makeText(requireContext(), "Permission needed!", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
 }
